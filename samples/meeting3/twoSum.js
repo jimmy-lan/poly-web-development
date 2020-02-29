b1bed89aae8289d6c7b3eb2e40eb2523358e1182
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 10:43:42 -0500

    Update requirement for twoSum.js

--- samples/meeting3/twoSum.js
+++ samples/meeting3/twoSum.js
@@ -6,6 +6,9 @@
  * Example: arr = [5, 6, 4, 4] target = 8 => return true since 4 + 4 = 8
  * Example: arr = [5, 6, 4, 4] target = 4 => return false since no two elements
  * in arr sum up to 4
+ * 
+ * Requirement: The expected running time of your algorithm should be O(n)
+ * 
  */
 exports.twoSum = (arr=[], target=0) => {
   
