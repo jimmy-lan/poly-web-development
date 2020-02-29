ab03e674fad5de35e0c2e7eb0b72e492268381aa
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 14:29:44 -0500

    Complete test for longest substring

--- samples/meeting3/longestSubstring.js
+++ samples/meeting3/longestSubstring.js
@@ -5,11 +5,15 @@
 */
 
 /**
- * Return the longest substring without repeating characters in s.
+ * Return the length of the longest substring without repeating characters
+ * in s.
  * 
- * Example: s = "abcabcbb", the function should return "abc"
- * Example: s = "bbb", the function should return "b"
- * Example: s = "ututt", the function should return "ut"
+ * Example: s = "abcabcbb", the function should return 3 since the
+ * required substring is "abc"
+ * Example: s = "bbb", the function should return 1 since the required substring
+ * is "b"
+ * Example: s = "ututt", the function should return 2 since the required
+ * substring is "ut"
  * 
  * Requirement: This algorithm should run in O(n) time.
  * Hint: Try to come up with an algorithm that does this in O(n^2) time,
--- samples/meeting3/longestSubstring.test.js
+++ samples/meeting3/longestSubstring.test.js
@@ -1,15 +1,27 @@
 const { longestSubstring } = require('./longestSubstring');
 
 describe('longestSubstring', () => {
-  test('returns an empty string when s = ""', () => {
-    expect(longestSubstring("")).toBe("");
+  test('returns 0 when s is an empty string', () => {
+    expect(longestSubstring("")).toBe(0);
   });
 
-  test('returns "a" when s = "a"', () => {
-    expect(longestSubstring("a")).toBe("a");
+  test('returns 1 when s = "a"', () => {
+    expect(longestSubstring("a")).toBe(1);
   });
 
-  test('returns "a" when s = "aaaaa"', () => {
-    expect(longestSubstring("aaaaa")).toBe("a");
+  test('returns 1 when s = "aaaaa"', () => {
+    expect(longestSubstring("aaaaa")).toBe(1);
+  });
+
+  test('returns "3 when s = "babcbabb"', () => {
+    expect(longestSubstring("babcbabb")).toBe(3);
+  });
+
+  test('returns 5 when s = "abcbxyzrx"', () => {
+    expect(longestSubstring("abcbxyzrx")).toBe(5);
+  });
+
+  test('returns 2 when s = "bbbbbab"', () => {
+    expect(longestSubstring("bbbbbab")).toBe(2);
   })
 });
