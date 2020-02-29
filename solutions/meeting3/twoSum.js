c0ff7733160a8c5f1c5fe09aed2378517d97a67b
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 13:36:46 -0500

    Update solutions/meeting3

--- solutions/meeting3/twoSum.js
+++ solutions/meeting3/twoSum.js
@@ -13,6 +13,13 @@
 exports.twoSum = (arr=[], target=0) => {
   if (arr.length < 2) return false;
 
+  /* idea: traverse through arr and see if I have seen (target) - (current element)
+  before. If I have seen it, we know (target) - (current element) + (current element)
+  equals target, so return true. Otherwise, record the current element.
+  The key here is that we need a data structure that performs insert and lookup in 
+  constant time. A hash table is a good choice. Note that there are other 
+  implementations of this problem as well.
+  */
   let seenElements = {};
   for (el of arr) {
     if (seenElements[target-el]) {
