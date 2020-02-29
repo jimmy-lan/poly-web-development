eb88dd9ca90eeff44c4aa2c14fbaf38f0959ed6c
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 14:06:49 -0500

    Add longestSubstring

---
+++ samples/meeting3/longestSubstring.js
@@ -0,0 +1,20 @@
+/*
+* This file contains a problem that is slightly more difficult compared to 
+* twoSum. This problem is *optional* for this week and will be discussed
+* during our third meeting. You may attempt the problem before the meeting.
+*/
+
+/**
+ * Return the longest substring without repeating characters in s.
+ * 
+ * Example: s = "abcabcbb", the function should return "abc"
+ * Example: s = "bbb", the function should return "b"
+ * Example: s = "ututt", the function should return "ut"
+ * 
+ * Requirement: This algorithm should run in O(n) time.
+ * Hint: Try to come up with an algorithm that does this in O(n^2) time,
+ * then think about ways to optimize it.
+ */
+exports.longestSubstring = (s) => {
+
+}
