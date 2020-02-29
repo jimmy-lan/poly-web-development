5a87459acb4d8059b664be069929c090dd9a04c9
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 13:30:04 -0500

    Create solutions folder

---
+++ solutions/meeting3/twoSum.js
@@ -0,0 +1,24 @@
+
+/**
+ * Return true if there exists two elements in arr that sums up
+ * to target.
+ * 
+ * Example: arr = [5, 6, 4, 4] target = 8 => return true since 4 + 4 = 8
+ * Example: arr = [5, 6, 4, 4] target = 4 => return false since no two elements
+ * in arr sum up to 4
+ * 
+ * Requirement: The expected running time of your algorithm should be O(n)
+ * 
+ */
+exports.twoSum = (arr=[], target=0) => {
+  if (arr.length < 2) return false;
+
+  let seenElements = {};
+  for (el of arr) {
+    if (seenElements[target-el]) {
+      return true;
+    }
+    seenElements[el] = true;
+  }
+  return false;
+}
