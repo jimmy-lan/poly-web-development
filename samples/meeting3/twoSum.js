8a6fa4e3b46a1f2e5af8b468bf309f49a54b618c
Jimmy <jimmylanbcn@gmail.com>
2020-02-29 10:40:45 -0500

    Add twosum function

---
+++ samples/meeting3/twoSum.js
@@ -0,0 +1,12 @@
+
+/**
+ * Return true if there exists two elements in arr that sums up
+ * to target.
+ * 
+ * Example: arr = [5, 6, 4, 4] target = 8 => return true since 4 + 4 = 8
+ * Example: arr = [5, 6, 4, 4] target = 4 => return false since no two elements
+ * in arr sum up to 4
+ */
+exports.twoSum = (arr=[], target=0) => {
+  
+}
