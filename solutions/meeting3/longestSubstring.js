c10a89d52c5b1695acbd4907eb3a530d9ea9604a
Jimmy <jimmylanbcn@gmail.com>
2020-03-06 16:02:02 -0500

    Update

---
+++ solutions/meeting3/longestSubstring.js
@@ -0,0 +1,40 @@
+/*
+* This file contains a problem that is slightly more difficult compared to 
+* twoSum. This problem is *optional* for this week and will be discussed
+* during our third meeting. You may attempt the problem before the meeting.
+*/
+
+/**
+ * Return the length of the longest substring without repeating characters
+ * in s.
+ * 
+ * Example: s = "abcabcbb", the function should return 3 since the
+ * required substring is "abc"
+ * Example: s = "bbb", the function should return 1 since the required substring
+ * is "b"
+ * Example: s = "ututt", the function should return 2 since the required
+ * substring is "ut"
+ * 
+ * Requirement: This algorithm should run in O(n) time.
+ * Hint: Try to come up with an algorithm that does this in O(n^2) time,
+ * then think about ways to optimize it.
+ */
+exports.longestSubstring = (s) => {
+  let i = 0;
+  let j = 0;
+  let result = 0;
+  let n = s.length;
+  let window = {};
+  while (i < n && j < n) {
+    if (s[j] in window) {
+      delete window[s[i]];
+      i++;
+    } else {
+      window[s[j]] = true;
+      j++;
+      result = Math.max(result, j - i);
+    }
+  }
+
+  return result;
+}
---
+++ solutions/meeting4/validSudoku.js
@@ -0,0 +1,25 @@
+/**
+ * Check if board contains a valid sudoku.
+ * Board is a 9*9 2D array of characters. Valid characters in board includes
+ * the characters '1' to '9' and '.'. A '.' character indicates an empty
+ * cell in the sudoku. Your job is to check whether the input board is valid.
+ * 
+ * We say that a sudoku board is valid if and only if:
+ * 1. Each row contains digits 1-9 without repetition
+ * 2. Each column contains digits 1-9 without repetition
+ * 3. Each 3*3 subboxes contains digits 1-9 without repetition
+ * (Recall our discussion in meeting 4)
+ * 
+ * A board can be *valid* even if it is not *solvable*. Only filled cells
+ * (i.e. cells with characters from '1' to '9') needs to be validated, so
+ * ignore empty cells.
+ * 
+ * Note: if this description is not clear enough for you to solve the problem,
+ * recall our discussion during meeting 4.
+ * 
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+exports.validSudoku = function (board) {
+
+}
